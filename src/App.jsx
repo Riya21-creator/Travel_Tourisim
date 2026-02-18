1745d76c360c99ef40d61c5d9bf74f2485f3f081
Riya Mishra <2k22.cse.2213354@gmail.com>
2026-02-18 23:16:57 +0530

    test render home

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,7 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./components/Navbar";
-
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Destinations from "./pages/Destinations";
-import Packages from "./pages/Packages";
-import Booking from "./pages/Booking";
-import Contact from "./pages/Contact";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Payment from "./pages/Payment";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/destinations" element={<Destinations />} />
-        <Route path="/packages" element={<Packages />} />
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/payment" element={<Payment />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
 
 export default App;
